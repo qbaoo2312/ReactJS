0425c7e8fe355b69cf7fc2191756f3e74f243eec
qbaoo2312 <baonqph26602@fpt.edu.vn>
2023-07-17 19:00:51 +0700

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Calculate from './components/calculate';
 import Show from './components/show';
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+         <Counter></Counter>
          <Calculate></Calculate>
          <Show></Show>
       </header>
